18c3e9e75f734f44563828c27b7a306c82f5633c
sdrrv <duartecelvas@gmail.com>
2021-03-25 15:33:16 +0000

    first iteration

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Vertice{
+private:
+    int discovery_time;
+    std::vector <Vertice> vertices;
+};
+
+int main(){
+    return 0;
+}
+ 
