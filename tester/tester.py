a9fe66e0fcb257e542e11d2c645cd6119adec579
sdrrv <duartecelvas@gmail.com>
2021-04-02 18:16:27 +0100

    testing done and done

--- tester/tester.py
+++ tester/tester.py
@@ -1,23 +1,39 @@
 import os
-algo = r""
-testingCode = r""
-randomgen = r""
+algo = r"./mainv2.cpp"
+testingCode = r"./projeto1.cpp"
+randomGen = r"./randomDAG.cpp"
+
+direct = "./data"
 
 
 def compile():
+    print("COMPILING" + "-#"*30)
     # algo compiler
     os.system(f"g++ -std=c++11 -O3 -Wall {algo} -lm -o algo.out")
     # testingCode compiler
     os.system(f"g++ -std=c++11 -O3 -Wall {testingCode} -lm -o testingCode.out")
+    # randomGen compiler
+    os.system(f"g++ -std=c++11 -O3 -Wall {randomGen} -lm -o randomGen.out")
+    print("-#"*30)
+
+
+def populateInput(testInput, vertices, connections):
+    os.system(f"g++ -std=c++11 -O3 -Wall {randomGen} -lm -o randomGen.out")
+    os.system(f"./randomGen.out {vertices} {connections} > {testInput}")
 
 
 def test(iter, vertices, connections):
     compile()
+    os.system(f"touch {direct}/Algo.out")
+    os.system(f"touch {direct}/TestingCode.out")
     for i in range(iter):
-        direct = (f"./data/{i}")
-        testInput = (direct+"/"+"Input.in")
-        os.system(f"mkdir {direct}")
+        print(i)
+        testInput = (direct+"/"+f"Input{i}.in")
         os.system(f"touch {testInput}")
+        populateInput(testInput, vertices, connections)
+        os.system(f"./algo.out < {testInput} >> {direct}/Algo.out")
+        os.system(
+            f"./testingCode.out < {testInput} >> {direct}/TestingCode.out")
 
 
 def main():
